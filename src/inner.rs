bb49ed461bb5c7c53f8ce453368ee23de1e336cd
Mara Dominguez <maradominguez445@gmail.com>
2024-09-17 19:29:28 -0400

    working version that I think, needs tests + tokio loom

--- src/inner.rs
+++ src/inner.rs
@@ -1,93 +1,118 @@
 use std::cell::UnsafeCell;
-use std::ptr::NonNull;
+use std::mem::MaybeUninit;
 use std::sync::atomic::AtomicBool;
-use std::sync::atomic::AtomicU32;
+use std::sync::atomic::AtomicPtr;
 use std::sync::atomic::Ordering;
 
-pub struct InnerLock<T: Sync> {
-    data: UnsafeCell<T>,
-    locked: AtomicBool,
+pub(crate) struct RcuInner<T: Sync> {
+    left: UnsafeCell<MaybeUninit<T>>,
+    right: UnsafeCell<MaybeUninit<T>>,
+    use_left: AtomicBool,
+    both_init: AtomicBool,
+    writer_locked: AtomicBool,
+    cur: AtomicPtr<T>,
 }
 
-impl<T: Sync> InnerLock<T> {
-    pub fn lock(&self) -> InnerGuard<T> {
+impl<'a, T: Sync> RcuInner<T> {
+    pub(crate) fn new(data: T) -> Self {
+        let mut left = MaybeUninit::new(data);
+        let cur: AtomicPtr<T> = left.as_mut_ptr().into();
+        let left = UnsafeCell::new(left);
+        Self {
+            left,
+            right: MaybeUninit::uninit().into(),
+            use_left: true.into(),
+            both_init: false.into(),
+            writer_locked: false.into(),
+            cur,
+        }
+    }
+
+    pub(crate) fn try_lock(&'a self) -> Option<InnerGuard<'a, T>> {
+        if self
+            .writer_locked
+            .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+            .is_ok()
+        {
+            Some(InnerGuard { lock: self })
+        } else {
+            None
+        }
+    }
+
+    pub(crate) fn lock(&'a self) -> InnerGuard<'a, T> {
         self.spin();
         InnerGuard { lock: self }
     }
 
-    pub(crate) fn try_lock(&self) -> Option<InnerGuard<T>> {
-        self.locked
-            .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
-            .map(|_| InnerGuard { lock: self }).ok()
+    // safe because cur always points to the newest data
+    fn get_cur(&self) -> &T {
+        unsafe { self.cur.load(Ordering::SeqCst).as_ref() }.unwrap()
     }
 
     fn spin(&self) {
         while self
-            .locked
+            .writer_locked
             .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
             .is_err()
         {}
     }
+
+    #[allow(clippy::mut_from_ref)]
+    // only safe if we have exclusive reference (via reader lock)
+    unsafe fn left_mut(&self) -> &mut MaybeUninit<T> {
+        unsafe { self.left.get().as_mut().unwrap() }
+    }
+
+    #[allow(clippy::mut_from_ref)]
+    unsafe fn right_mut(&self) -> &mut MaybeUninit<T> {
+        unsafe { self.right.get().as_mut().unwrap() }
+    }
+
+    // only safe if we have the reader lock
+    pub(crate) unsafe fn update(&self, new: T) -> Result<(), T> {
+        if self.both_init.load(Ordering::Acquire) {
+            Err(new)
+        } else {
+            let new = MaybeUninit::new(new);
+            // if use left is true then we're overwriting right and vice versa
+            let ptr = if self.use_left.fetch_not(Ordering::SeqCst) {
+                unsafe { self.right_mut() }
+            } else {
+                unsafe { self.left_mut() }
+            };
+            *ptr = new;
+            self.cur.store(ptr.as_mut_ptr(), Ordering::Release);
+            Ok(())
+        }
+    }
 }
 
-impl<T: Sync> AsRef<T> for InnerLock<T> {
+impl<T: Sync> AsRef<T> for RcuInner<T> {
     fn as_ref(&self) -> &T {
-        unsafe { self.data.get().as_ref() }.unwrap()
+        self.get_cur()
     }
 }
 
 pub struct InnerGuard<'a, T: Sync> {
-    lock: &'a InnerLock<T>,
+    lock: &'a RcuInner<T>,
 }
 
-impl<T: Sync> Drop for InnerGuard<'_,T> {
+impl<T: Sync> Drop for InnerGuard<'_, T> {
     fn drop(&mut self) {
-        self.lock.locked.store(false, Ordering::Release)
+        self.lock.writer_locked.store(false, Ordering::Release)
     }
 }
 
-impl<'a, T: Sync> AsRef<T> for InnerGuard<'a,T> {
+impl<'a, T: Sync> AsRef<T> for InnerGuard<'a, T> {
     fn as_ref(&self) -> &'a T {
-        unsafe{self.lock.data.get().as_ref().unwrap()}
-    }
-}
-impl<T: Sync> AsMut<T> for InnerGuard<'_, T> {
-    fn as_mut(&mut self) -> &mut T {
-        unsafe { self.lock.data.get().as_mut().unwrap() }
+        self.lock.get_cur()
     }
 }
 
-pub struct RcuReadGuard<'a, T> {
-    data: NonNull<T>,
-    readers: &'a AtomicU32,
-}
-
-impl<'a, T: Sync> RcuReadGuard<'a, T> {
-    pub fn new(data: &T, readers: &'a AtomicU32) -> Self {
-        Self {
-            data: data.into(),
-            readers,
-        }
-    }
-}
-
-impl<T> AsRef<T> for RcuReadGuard<'_, T> {
-    fn as_ref(&self) -> &T {
-        unsafe { self.data.as_ref() }
-    }
-}
-
-impl<T> Drop for RcuReadGuard<'_, T> {
-    fn drop(&mut self) {
-        self.readers.fetch_sub(1, Ordering::AcqRel);
-    }
-}
-
-impl<T: Sync> From<T> for InnerLock<T> {
-    fn from(data: T) -> Self {
-        Self {
-            data: UnsafeCell::new(data),
-            locked: false.into(),
-        }
+impl<'a, T: Sync> InnerGuard<'a, T> {
+    pub(crate) fn update(&self, new: T) -> Result<(), T> {
+        // safe because by definition we have the lock
+        unsafe { self.lock.update(new) }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,136 +1,142 @@
+use std::future::Future;
+use std::pin::Pin;
 use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering;
+use std::task::Context;
+use std::task::Poll;
 
 mod r#async;
 mod inner;
 
 use inner::InnerGuard;
-use inner::InnerLock;
-use inner::RcuReadGuard;
+use inner::RcuInner;
 
 pub struct NeedsReclaim();
 
 pub struct Rcu<T: Sync> {
-    data: RcuData<T>,
+    inner: RcuInner<T>,
     readers: AtomicU32,
 }
 
 impl<'a, T: Sync> Rcu<T> {
     pub fn new(data: T) -> Self {
         Self {
-            data: (data, None).into(),
+            inner: RcuInner::new(data),
             readers: 0.into(),
         }
     }
 
     pub fn read(&self) -> RcuReadGuard<'_, T> {
         self.readers.fetch_add(1, Ordering::Acquire);
-        let readers = &self.readers;
-        let data = self.next_data().as_ref().unwrap_or_else(|| self.current_data());
-        RcuReadGuard::new(data, readers)
+        RcuReadGuard { lock: self }
     }
 
-
-    // There are 3 stages to writing in the RCU, update, synchronize, and reclaim:
-    // - Update provides a new value to the RCU
-    // - synchronize ensures that there are no references to the old data
-    // - reclaim frees whatever memory the old data was pointing to
-    //
-    // I provide the raw interfaces to update, synchronize, and reclaim via update_locked,
-    // otherwise update_now
-
-    // Spin until there are no readers left, and then free old value
-    // Doesn't matter that new readers can show up, all new readers will get new data
-    // FIXME in the kernel, the user handles memory and so it's trivial that next_data and data
-    // never move, however in this implementation, that's not the case. Maybe use Pin for this?
-    // or like two MaybeUninit<T>s with a bool to indicate which one we're
-    // currently using? idk that sucks ngl
-    // perhaps you clone T and store it twice initially, arbitrarily decide ones the starting one
-    // and go from there
-    //
-    pub fn synchronize(&self) {
-        while self.get_state() != RcuState::Synchronized {}
-    }
-
-    // just update our value
-    pub fn update_locked(
-        &self,
-        data: T,
-        lock: &mut InnerGuard<(T, Option<T>)>,
-    ) -> Result<(), NeedsReclaim> {
-        let new_data = Self::next_mut(lock);
-        if self.get_state() == RcuState::TwoRep {
-            return Err(NeedsReclaim {});
+    pub fn update_now(&mut self, new: T) {
+        if let Err(value) = unsafe { self.inner.update(new) } {
+            self.synchronize();
+            self.update_now(value);
         }
-        let _ = std::mem::replace(new_data, Some(data));
-        Ok(())
-    }
-
-    // updates, synchronize, then reclaims, all in order
-    // note this blocks twice, first to get the updater lock, and then again to synchronize
-    pub fn update_now(&self, new: T) {
-        let mut lock = self.data.lock();
-        let data = Self::next_mut(&mut lock);
-        let _ = std::mem::replace(data, Some(new));
         self.synchronize();
-        unsafe {Self::raw_reclaim(&mut lock)};
     }
 
-    pub fn reclaim(&self) -> Option<T> {
-        let mut data = self.data.lock();
-        if self.get_state() == RcuState::Synchronized {
-            unsafe {
-                Self::raw_reclaim(&mut data)
-            }
+    pub async fn update_later(&self, new: T) {
+        let lock = (&self.inner).await;
+        if let Err(new) = lock.update(new) {
+            drop(lock);
+            self.asynchronize().await;
+            self.update_later(new).await
         } else {
-            None
+            self.asynchronize().await;
         }
     }
 
-
-    /// # Safety 
-    /// safe iff there are no existing references to data or next_data (i.e. this should be called immediately after
-    /// synchronize
-    pub unsafe fn raw_reclaim(lock: &mut InnerGuard<'_, (T, Option<T>)>) -> Option<T>{
-        let (data, new_data) = lock.as_mut();
-            new_data
-                .take()
-                .map(|new_data| std::mem::replace(data, new_data))
+    pub async fn asynchronize(&self) {
+        self.await
     }
 
-
-    fn _current_mut(lock:&'a mut InnerGuard<'_, (T, Option<T>)>) -> &'a mut T{
-        &mut lock.as_mut().0
+    // block until all readers are reading the same data
+    fn synchronize(&self) {
+        while self.readers.load(Ordering::Relaxed) != 0 {}
     }
 
-    fn next_mut(lock:&'a mut InnerGuard<'_, (T, Option<T>)>) -> &'a mut Option<T>{
-        &mut lock.as_mut().1
-    }
-
-    fn current_data(&self) -> &T {
-        &self.data.as_ref().0
-    }
-
-    fn next_data(&self) -> &Option<T> {
-        &self.data.as_ref().1
-    }
-
-    fn get_state(&self) -> RcuState {
-        if self.readers.load(Ordering::Acquire) == 0 {
-            RcuState::Synchronized
-        } else if self.next_data().is_some() {
-            RcuState::TwoRep
-        } else {
-            RcuState::OneRep
+    // lock writes to self, now the user needs to synchronize and reclaim, but they can do it at
+    // their leisure
+    pub fn write(&'a self) -> RcuWriteGuard<'a, T> {
+        let inner_lock = self.inner.lock();
+        RcuWriteGuard {
+            lock: self,
+            inner: inner_lock,
         }
     }
 }
 
-pub type RcuData<T> = InnerLock<(T, Option<T>)>;
+pub struct RcuWriteGuard<'a, T: Sync> {
+    lock: &'a Rcu<T>,
+    inner: InnerGuard<'a, T>,
+}
 
-#[derive(PartialEq)]
-enum RcuState {
-    OneRep,
-    TwoRep,
-    Synchronized,
+impl<'a, T: Sync> RcuWriteGuard<'a, T> {
+    pub fn update(&self, new: T) -> Result<(), T> {
+        self.inner.update(new)
+    }
+
+    pub fn synchronize(&self) {
+        self.lock.synchronize();
+    }
+}
+
+impl<'a, T: Sync> AsRef<T> for RcuWriteGuard<'a, T> {
+    fn as_ref(&self) -> &T {
+        self.inner.as_ref()
+    }
+}
+
+pub struct RcuReadGuard<'a, T: Sync> {
+    lock: &'a Rcu<T>,
+}
+
+impl<T: Sync> AsRef<T> for RcuReadGuard<'_, T> {
+    fn as_ref(&self) -> &T {
+        self.lock.inner.as_ref()
+    }
+}
+
+impl<T: Sync> Drop for RcuReadGuard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.readers.fetch_sub(1, Ordering::AcqRel);
+    }
+}
+
+impl<T: Sync> Future for Rcu<T> {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.readers.load(Ordering::Acquire) > 0 {
+            Poll::Pending
+        } else {
+            Poll::Ready(())
+        }
+    }
+}
+
+// polling on Rcu means waiting until there are no readers
+impl<T: Sync> Future for &Rcu<T> {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.readers.load(Ordering::Acquire) > 0 {
+            Poll::Pending
+        } else {
+            Poll::Ready(())
+        }
+    }
+}
+
+// polling on inner means getting the write lock
+impl<'a, T: Sync> Future for &'a RcuInner<T> {
+    type Output = InnerGuard<'a, T>;
+    fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.try_lock() {
+            None => Poll::Pending,
+            Some(guard) => Poll::Ready(guard),
+        }
+    }
 }
