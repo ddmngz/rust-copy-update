f20834f9991f6988777e7e1f1bf897e1d9c2af48
Mara Dominguez <maradominguez445@gmail.com>
2024-09-18 03:03:21 -0400

    evil rcu,, she's beautiful

--- src/evil_rcu.rs
+++ src/evil_rcu.rs
@@ -2,46 +2,110 @@
 //
 //
 use std::sync::Arc;
-struct EvilRcu<T:Sync>(Vec<Arc<T>>);
+use std::sync::Mutex;
+use std::sync::MutexGuard;
+use std::cell::UnsafeCell;
 
-struct RcuReadGuard<T>(Arc<T>);
+struct EvilRcu<T:Sync>{
+    newest: EvilRcuInner<T>,
+    vestiges: Mutex<Vec<Arc<T>>>,
+}
 
-
-
-impl<T:Sync> EvilRcu<T>{
+impl<'a, T:Sync> EvilRcu<T>{
     fn new(data:T) -> Self{
+        Self{
+            newest:EvilRcuInner::new(Arc::new(data)),
+            vestiges:Mutex::new(Vec::new()),
+        }
+    }
+    
+    fn read(&'a self) -> RcuReadGuard<'a, T>{
+        let ref_count = self.newest.get_cloned();
+        RcuReadGuard{
+            ref_count,
+            lock:&self,
+        }
+    }
 
-        let mut inner = Vec::new();
-        inner.push(Arc::new(data));
-        Self(
-            inner
-        )
+    fn write(&'a self) -> RcuWriteGuard<'a, T>{
+        RcuWriteGuard{data: self.vestiges.lock().unwrap(), lock: &self} 
+    }
+
+    fn try_write(&'a self) -> Option<RcuWriteGuard<'a, T>>{
+        self.vestiges.try_lock().ok().map(|data|RcuWriteGuard{ data, lock: &self} )
+    }
+
+    fn update_now(&self, data:Arc<T>){
+        let old_news = self.newest.replace(data);
+        let mut lock = self.write();
+        lock.push(old_news);
+        Self::clean(&mut lock);
+        Self::synchronize(&mut lock);
+    }
+
+    fn update_lite(&self, data:Arc<T>){
+        let old_news = self.newest.replace(data);
+        let mut lock = self.write();
+        lock.push(old_news);
+        Self::clean(&mut lock);
     }
 
 
-    fn end(&self) -> &Arc<T>{
-        &self.0.get(self.0.len()).expect("Somehow Rcu ended up empty")
+    fn clean(guard:&mut RcuWriteGuard<T>){
+        guard.clean()
     }
 
-    fn read(&self) -> RcuReadGuard<T>{
-        RcuReadGuard(self.end().clone())
+    fn synchronize(guard:&mut RcuWriteGuard<T>){
+        guard.synchronize()
+    }
+}
+
+struct EvilRcuInner<T:Sync>(UnsafeCell<Arc<T>>);
+
+impl<T:Sync> EvilRcuInner<T>{
+    fn new(data:Arc<T>) -> Self{
+        Self(UnsafeCell::new(data))
     }
 
-    fn update(&mut self, data:T){
-        self.0.push(Arc::new(data))
+    fn get_cloned(&self) -> Arc<T>{
+        unsafe{self.0.get().as_ref()}.unwrap().clone()
+    }
+
+    fn replace(&self, new:Arc<T>) -> Arc<T>{
+        std::mem::replace(unsafe{self.0.get().as_mut()}.unwrap(), new)
+    }
+}
+
+
+struct RcuReadGuard<'a, T:Sync>{
+    ref_count:Arc<T>,
+    lock:&'a EvilRcu<T>
+}
+
+
+struct RcuWriteGuard<'a, T:Sync>{
+    data:MutexGuard<'a, Vec<Arc<T>>>,
+    lock: &'a EvilRcu<T>,
+}
+
+impl<T:Sync> RcuWriteGuard<'_,T>{
+    fn push(&mut self, data:Arc<T>){
+        self.data.push(data)
     }
 
     fn clean(&mut self){
-        // make sure we don't accidentally delete everything
-        let safety_guard = self.end().clone();
-        self.0.retain(|x| Arc::strong_count(x) >=1);   
-        drop(safety_guard);
+        self.data.retain(|x| Arc::strong_count(x) >=1);   
     }
 
     fn synchronize(&mut self){
-        while self.0.len() > 1{
+        while self.data.len() > 1{
             self.clean();
         }
     }
+
+    fn update(&mut self, new:T){
+        self.lock.newest.replace(Arc::new(new));
+    }
+
 }
 
