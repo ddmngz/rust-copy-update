337486519b07b8ad2396211e6f326016a6abb6bb
Mara Dominguez <maradominguez445@gmail.com>
2024-09-21 12:39:39 -0400

    finished implementing trait on evil rcu

--- src/evil_rcu.rs
+++ src/evil_rcu.rs
@@ -1,66 +1,51 @@
 //this is so fucked up wth
 
 use std::cell::UnsafeCell;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
-use std::sync::Mutex;
-use std::sync::MutexGuard;
+
+use crate::async_ext::AsyncRcu;
+use crate::mut_rcu::MutRcu;
+use crate::Rcu;
+use crate::RcuWriteGuard;
 
 struct EvilRcu<T: Sync> {
     newest: EvilRcuInner<T>,
-    vestiges: Mutex<Vec<Arc<T>>>,
+    locked: AtomicBool,
 }
 
-impl<'a, T: Sync> EvilRcu<T> {
+impl<'a, T: Sync + 'a> Rcu<'a, T> for EvilRcu<T> {
+    type ReadGuard = EvilReadGuard<T>;
+    type WriteGuard = EvilWriteGuard<'a, T>;
+    fn read(&'a self) -> EvilReadGuard<T> {
+        let inner = self.newest.get_cloned();
+        EvilReadGuard { inner }
+    }
+
+    fn write(&'a self) -> EvilWriteGuard<T> {
+        self.lock();
+        EvilWriteGuard { lock: self }
+    }
+    fn synchronize(&self) {
+        while self.newest.ref_count() > 1 {}
+    }
+}
+
+impl<'a, T: Sync + 'a> EvilRcu<T> {
     fn new(data: T) -> Self {
         Self {
             newest: EvilRcuInner::new(Arc::new(data)),
-            vestiges: Mutex::new(Vec::new()),
+            locked: false.into(),
         }
     }
 
-    fn read(&'a self) -> RcuReadGuard<'a, T> {
-        let ref_count = self.newest.get_cloned();
-        RcuReadGuard {
-            ref_count,
-            lock: &self,
-        }
-    }
-
-    fn write(&'a self) -> RcuWriteGuard<'a, T> {
-        RcuWriteGuard {
-            data: self.vestiges.lock().unwrap(),
-            lock: &self,
-        }
-    }
-
-    fn try_write(&'a self) -> Option<RcuWriteGuard<'a, T>> {
-        self.vestiges
-            .try_lock()
-            .ok()
-            .map(|data| RcuWriteGuard { data, lock: &self })
-    }
-
-    fn update_now(&self, data: Arc<T>) {
-        let old_news = self.newest.replace(data);
-        let mut lock = self.write();
-        lock.push(old_news);
-        Self::clean(&mut lock);
-        Self::synchronize(&mut lock);
-    }
-
-    fn update_lite(&self, data: Arc<T>) {
-        let old_news = self.newest.replace(data);
-        let mut lock = self.write();
-        lock.push(old_news);
-        Self::clean(&mut lock);
-    }
-
-    fn clean(guard: &mut RcuWriteGuard<T>) {
-        guard.clean()
-    }
-
-    fn synchronize(guard: &mut RcuWriteGuard<T>) {
-        guard.synchronize()
+    fn lock(&self) {
+        while self
+            .locked
+            .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+            .is_err()
+        {}
     }
 }
 
@@ -85,34 +70,93 @@ impl<T: Sync> EvilRcuInner<T> {
     fn replace(&self, new: Arc<T>) -> Arc<T> {
         unsafe { self.0.get().replace(new) }
     }
+
+    // SAFETY same safety as get_cloned
+    fn ref_count(&self) -> usize {
+        Arc::strong_count(unsafe { self.0.get().as_ref() }.unwrap())
+    }
 }
 
-struct RcuReadGuard<'a, T: Sync> {
-    ref_count: Arc<T>,
+struct EvilReadGuard<T: Sync> {
+    inner: Arc<T>,
+}
+
+impl<'a, T: Sync + 'a> AsRef<T> for EvilReadGuard<T> {
+    fn as_ref(&self) -> &T {
+        &self.inner
+    }
+}
+
+struct EvilWriteGuard<'a, T: Sync> {
     lock: &'a EvilRcu<T>,
 }
 
-struct RcuWriteGuard<'a, T: Sync> {
-    data: MutexGuard<'a, Vec<Arc<T>>>,
-    lock: &'a EvilRcu<T>,
+impl<'a, T: Sync + 'a> RcuWriteGuard<'a, T> for EvilWriteGuard<'a, T> {
+    fn update_unsynced(&mut self, data: T) {
+        self.lock.newest.replace(Arc::new(data));
+    }
+
+    fn synchronize(&self) {
+        self.lock.synchronize();
+    }
 }
 
-impl<T: Sync> RcuWriteGuard<'_, T> {
-    fn push(&mut self, data: Arc<T>) {
-        self.data.push(data)
+impl<'a, T: Sync + 'a> Drop for EvilWriteGuard<'a, T> {
+    fn drop(&mut self) {
+        self.lock.locked.store(false, Ordering::Release)
     }
+}
 
-    fn clean(&mut self) {
-        self.data.retain(|x| Arc::strong_count(x) >= 1);
+impl<'a, T: Sync + 'a> MutRcu<'a, T> for EvilRcu<T> {
+    fn mut_write(&'a mut self) -> EvilWriteGuard<'a, T> {
+        self.locked.store(true, Ordering::Release);
+        EvilWriteGuard { lock: self }
     }
+}
 
-    fn synchronize(&mut self) {
-        while self.data.len() > 1 {
-            self.clean();
+use std::future::Future;
+use std::pin::Pin;
+use std::task::Context;
+use std::task::Poll;
+
+impl<'a, T: Sync + 'a> Future for &'a EvilRcu<T> {
+    type Output = &'a EvilRcu<T>;
+    fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
+        if self
+            .locked
+            .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+            .is_ok()
+        {
+            Poll::Ready(*self)
+        } else {
+            Poll::Pending
         }
     }
+}
 
-    fn update(&mut self, new: T) {
-        self.lock.newest.replace(Arc::new(new));
+impl<T: Sync> Future for &EvilRcuInner<T> {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.ref_count() == 1 {
+            Poll::Ready(())
+        } else {
+            Poll::Pending
+        }
+    }
+}
+
+impl<'a, T: Sync + 'a> AsyncRcu<'a, T> for EvilRcu<T> {
+    async fn poll_write(&'a self) -> Self::WriteGuard {
+        EvilWriteGuard { lock: self.await }
+    }
+
+    async fn poll_synchronize(&self) {
+        (&self.newest).await
+    }
+
+    async fn poll_update(&self, data: T) {
+        let mut lock = self.poll_write().await;
+        lock.update_unsynced(data);
+        self.poll_synchronize().await;
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,56 @@
 pub mod evil_rcu;
 
+//
+// interface goal
+// read is easy
+// use write to either update synced or update unsynced
+
 pub trait Rcu<'a, T: Sync + 'a> {
-    type RcuReadGuard;
-    type RcuWriteGuard;
-    fn read(&'a self) -> Self::RcuReadGuard;
+    type ReadGuard: AsRef<T>;
+    type WriteGuard: RcuWriteGuard<'a, T>;
 
-    fn write(&'a self) -> Self::RcuWriteGuard;
-
-    fn mut_write(&'a mut self) -> Self::RcuWriteGuard;
-
-    fn update_now(&self, data: T);
-
-    fn mut_update(&mut self, data: T);
-
-    fn synchronize(&self, guard: &mut Self::RcuWriteGuard);
+    fn read(&'a self) -> Self::ReadGuard;
+    fn write(&'a self) -> Self::WriteGuard;
+    fn synchronize(&self);
+    fn update_now(&'a self, data: T) {
+        let mut lock = self.write();
+        lock.update_synced(data);
+    }
 }
 
-mod async_ext {
+// ensure we're synchronized before updating
+trait RcuWriteGuard<'a, T: Sync + 'a> {
+    fn update_unsynced(&mut self, data: T);
+    fn synchronize(&self);
+    fn update_synced(&mut self, data: T) {
+        self.synchronize();
+        self.update_unsynced(data);
+    }
+}
+
+pub mod mut_rcu {
+    use super::*;
+    pub trait MutRcu<'a, T: Sync + 'a>: Rcu<'a, T> {
+        fn mut_write(&'a mut self) -> Self::WriteGuard;
+        fn mut_update_synced(&'a mut self, new: T) {
+            let mut lock = self.mut_write();
+            lock.update_synced(new);
+        }
+
+        fn mut_update_unsynced(&'a mut self, new: T) {
+            let mut lock = self.mut_write();
+            lock.update_unsynced(new);
+        }
+    }
+}
+
+pub mod async_ext {
     use super::*;
     use std::future::Future;
     pub trait AsyncRcu<'a, T: Sync + 'a>: Rcu<'a, T> {
-        fn poll_write(&'a self) -> impl Future<Output = Option<Self::RcuWriteGuard>>;
+        fn poll_write(&'a self) -> impl Future<Output = Self::WriteGuard>;
+
+        fn poll_synchronize(&self) -> impl Future<Output = ()>;
 
         fn poll_update(&self, data: T) -> impl Future<Output = ()>;
     }
